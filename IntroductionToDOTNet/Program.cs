ea616b98963d84aaa13e4ad13aae625219519b45
DmitryK777 <kislyakovdmv@gmail.com>
2024-07-21 16:22:28 +0300

    Class work

--- IntroductionToDOTNet/Program.cs
+++ IntroductionToDOTNet/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//#define CONSOLE_PARAMETERS
+//#define INPUT_DATA
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +12,34 @@ namespace IntroductionToDOTNet
 	{
 		static void Main(string[] args)
 		{
+			Console.Title = "Introduction to .NET";
+#if CONSOLE_PARAMETERS
+			Console.Beep(700, 500);
+			Console.BackgroundColor = ConsoleColor.DarkBlue;
+			//Console.CursorLeft = 22;
+			//Console.CursorTop = 11;
+			Console.SetCursorPosition(22, 7);
+			Console.Write("Hello .NET\n"); // Выводит строку на экран
+			Console.WriteLine("Introduction"); // Выводит строку на экран и переводит курсор в начало следующей строки
+			Console.BackgroundColor = ConsoleColor.Green;
+			Console.ResetColor();
+			//Console.Clear();
+#endif
 
-		}
+#if INPUT_DATA
+			Console.Write("Введите ваше имя: ");
+			string first_name = Console.ReadLine();
+			Console.Write("Введите вашу фамилию: ");
+			string last_name = Console.ReadLine();
+			Console.Write("Введите ваш возраст: ");
+			int age = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Ваше имя: " + first_name + ", Ваша фамилия: " + last_name + ", Ваш возраст: " + age); // Конкатенация строк
+            Console.WriteLine(string.Format("Ваше имя: {0}, Ваша фамилия: {1}, Ваш возраст: {2}", first_name, last_name, age)); // Форматирование строк
+			Console.WriteLine($"Ваше имя: {first_name}, Ваша фамилия: {last_name}, Ваш возраст: {age}"); // Интерполяция строк
+#endif
+
+
+        }
 	}
 }
