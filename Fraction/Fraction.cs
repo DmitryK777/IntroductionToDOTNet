26d00a4bacd361a7034d6d45aa781143c947a03d
DmitryK777 <kislyakovdmv@gmail.com>
2024-08-25 16:22:35 +0300

    Fraction in progress

---
+++ Fraction/Fraction.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+	internal class Fraction
+	{
+		public int Integer { get; set; }
+		public int Numerator { get; set; }
+		int denominator;
+		public int Denominator
+		{
+			get => denominator;
+			set
+			{
+				if (value == 0) value = 1;
+				denominator = value;
+			}
+		}
+
+		public Fraction()
+		{
+			Denominator = 1;
+            Console.WriteLine($"DefaultConstructor:\t {this.GetHashCode()}");
+        }
+
+		public Fraction(int integer)
+		{
+			Integer = integer;
+			Denominator = 1;
+			Console.WriteLine($"ConstructorOfInteger:\t {this.GetHashCode()}");
+		}
+
+		public Fraction(int numerator, int denominator)
+		{
+			Numerator = numerator;
+			Denominator = denominator;
+			Console.WriteLine($"ConstructorOfWithoutInteger:\t {this.GetHashCode()}");
+		}
+
+		public Fraction(int integer, int numerator, int denominator)
+		{
+			Integer = integer;
+			Numerator = numerator;
+			Denominator = denominator;
+			Console.WriteLine($"ConstructorOfFullFraction:\t {this.GetHashCode()}");
+		}
+
+		public Fraction(Fraction other)
+		{
+			this.Integer = other.Integer;
+			this.Numerator = other.Numerator;
+			this.Denominator = other.Denominator;
+			Console.WriteLine($"CopyConstructor:\t {this.GetHashCode()}");
+		}
+
+		~Fraction()
+		{
+			Console.WriteLine($"Destructor:\t {this.GetHashCode()}");
+		}
+
+		// Arithmetical Operators
+		public static Fraction operator*(Fraction left, Fraction right)
+		{
+			/*
+			Fraction left_copy = new Fraction(left);
+			Fraction right_copy = new Fraction(right);
+			left_copy.ToImproper();
+			right_copy.ToImproper();
+			*/
+			/*
+			Fraction left_copy = left.ToImproper();
+			Fraction right_copy = right.ToImproper();
+			return new Fraction
+				(
+					left_copy.Numerator * right_copy.Numerator,
+					left_copy.Denominator * right_copy.Denominator
+				).ToProper();
+			*/
+			return new Fraction
+				(
+					left.ToImproper().Numerator * right.ToImproper().Numerator,
+					left.ToImproper().Denominator * right.ToImproper().Denominator
+				);
+		}
+
+		public static Fraction operator/(Fraction left, Fraction right)
+		{ 
+			return left * right.Inverted();
+		}
+
+		// Comparison Operators
+		public static bool operator ==(Fraction left, Fraction right)
+		{
+			return left.ToImproper().Numerator * right.ToImproper().Denominator == left.ToImproper().Denominator * right.ToImproper().Numerator;
+		}
+		public static bool operator !=(Fraction left, Fraction right)
+		{
+			return !(left == right);
+		}
+
+
+		// Methods
+		public Fraction ToProper()
+		{
+			//Integer += Numerator / Denominator;
+			//Numerator %= Denominator;
+			//return this;
+			Fraction proper = new Fraction();
+			proper.Integer += Numerator / Denominator;
+			proper.Numerator = Numerator % Denominator;
+			proper.Denominator = Denominator;
+			return proper;
+		}
+		public Fraction ToImproper()
+		{
+			//Numerator += Integer * Denominator;
+			//Integer = 0;
+			//return this;
+			return new Fraction(Numerator + Integer * Denominator, Denominator);
+		}
+		public Fraction Inverted()
+		{
+			//Fraction inverted = new Fraction(this);
+			//inverted.ToImproper();
+			Fraction inverted = ToImproper();
+			(inverted.Numerator, inverted.Denominator) = (inverted.Denominator, inverted.Numerator);
+			
+			return inverted;
+		}
+
+		public void Print()
+		{
+			if (Integer != 0) Console.Write(Integer);
+			if (Numerator != 0)
+			{
+				if (Integer != 0) Console.Write("(");
+				Console.Write($"{Numerator}/{Denominator}");
+				if (Integer != 0) Console.Write(")");
+			}
+			else if (Numerator == 0 && Integer == 0) Console.WriteLine(0);
+            Console.WriteLine();
+        }
+	}
+}
---
+++ OOP/Point.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+	internal class Point
+	{
+		
+		double x;
+		double y;
+
+		public double X
+		{
+			get { return x; }
+			set
+			{
+				if (value > 100) value = 100;
+				x = value;
+			}
+		}
+
+		public double Y
+		{
+			get => y;
+			set { y = value; }
+		}
+
+		
+		public double GetX()
+		{
+			return x;
+		}
+		public double GetY()
+		{
+			return y;
+		}
+
+		public void SetX(double x)
+		{
+			this.x = x;
+		}
+		public void SetY(double y)
+		{
+			this.y = y;
+		}
+		
+
+		// Можно использовать автосвойства
+		//public double X { get; set; }
+		//public double Y { get; set; }
+
+		public Point(double x = 0, double y = 0)
+		{
+			X = x;
+			Y = y;
+            Console.WriteLine($"Constructor: \t {this.GetHashCode()}");
+        }
+
+		~Point()
+		{
+			Console.WriteLine($"Destructor: \t {this.GetHashCode()}");
+		}
+	}
+	
+}
